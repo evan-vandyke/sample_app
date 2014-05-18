251e120e196f87b5f7b29b5f20091d42740d5b9e
Evan VanDyke <vandyke09@gmail.com>
2014-05-17 20:32:35 -0400

    added about to static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,18 @@ describe "Static pages" do
     end
   end
 
-  describe "Help Pages" do
+  describe "Help page" do
 
   	it "should have the content 'Help' " do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us' " do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
   end
 end
